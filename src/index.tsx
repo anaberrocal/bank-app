97b7b689c8f180806ba5d1563863e7e668ed512d
Ana Berrocal <anaberrocaldev@gmail.com>
2022-09-29 00:29:19 -0400

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,10 +6,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(<App />
 );
 
 // If you want to start measuring performance in your app, pass a function
